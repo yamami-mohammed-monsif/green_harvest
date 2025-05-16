40253ee7593ebd2b018dc0059c11f7b6a02af8b3
yamami mohamed monsif <mohammedyamami05@gmail.com>
2025-05-16 20:23:59 +0100

    updated the desktop nav - and added some animations

--- components/header/DesktopNav.tsx
+++ components/header/DesktopNav.tsx
@@ -1,23 +1,39 @@
+"use client";
+
 import { NavLinks } from "@/constants";
 import Link from "next/link";
+import { motion, MotionProvider } from "../utils/MotionProvider";
+import { menuVariants, menuItemVariants } from "@/constants/animations";
 
 const DesktopNav = () => {
   return (
-    <nav className="hidden lg:flex items-center gap-2">
-      {NavLinks.map((item) => {
-        return (
-          <Link
-            key={item.id}
-            href={item.href}
-            className="p-3 border border-neutral-white rounded-xl bg-transparent text-white hover:bg-white hover:text-neutral-green transition-all duration-300"
-          >
-            {item.label}
-          </Link>
-        );
-      })}
-      <button className="uppercase ml-5 bg-primary text-white py-4 px-8 rounded-4xl cursor-pointer hover:scale-110 transition-all duration-300">
-        shop now
-      </button>
+    <nav className="hidden lg:flex itmes-center">
+      <MotionProvider>
+        <motion.ul
+          variants={menuVariants}
+          initial="closed"
+          animate="open"
+          className="flex items-center gap-2"
+        >
+          {NavLinks.map((item) => {
+            return (
+              <motion.li variants={menuItemVariants} key={item.id}>
+                <Link
+                  href={item.href}
+                  className="p-3 border border-neutral-white rounded-xl bg-transparent text-white hover:bg-white hover:text-neutral-green transition-all duration-300"
+                >
+                  {item.label}
+                </Link>
+              </motion.li>
+            );
+          })}
+          <motion.li variants={menuItemVariants}>
+            <button className="uppercase ml-5 bg-primary text-white py-4 px-8 rounded-4xl cursor-pointer hover:scale-110 transition-all duration-300">
+              shop now
+            </button>
+          </motion.li>
+        </motion.ul>
+      </MotionProvider>
     </nav>
   );
 };
